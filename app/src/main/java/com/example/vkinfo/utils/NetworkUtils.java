360a69c339639dc8655a455d1e05aed3e3a6226c
Denis Besfamilnyy <d.besfamilnyy@niitp.in>
2019-08-28 11:06:32 +0300

    Added token string to generateUrl method

--- app/src/main/java/com/example/vkinfo/utils/NetworkUtils.java
+++ app/src/main/java/com/example/vkinfo/utils/NetworkUtils.java
@@ -2,7 +2,6 @@ package com.example.vkinfo.utils;
 
 import android.net.Uri;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -10,25 +9,22 @@ public class NetworkUtils {
 
     private static final String VK_API_BASE_URL = "https://api.vk.com/";
     private static final String VK_USERS_GET = "method/users.get";
+    private static final String VK_ACCESS_TOKEN_URL ="access_token";
     private static final String VK_API_TOKEN = "";
     private static final String PARAM_USER_ID = "user_ids";
     private static final String PARAM_VERSION = "v";
 
     /* генерирует URL на основании id который пришел от пользователя */
     public static URL generateURL(String userId) throws MalformedURLException {
-        Uri builtUri = Uri.parse(VK_API_BASE_URL + VK_USERS_GET) //TODO: add VK_API_TOKEN constant to concatenate string.
+        Uri builtUri = Uri.parse(VK_API_BASE_URL + VK_USERS_GET)
                 .buildUpon() //надстройка для добавления параметров к URI
                 //добавление параметра в надстройку
                 .appendQueryParameter(PARAM_USER_ID, userId)
                 .appendQueryParameter(PARAM_VERSION, "5.8")
+                .appendQueryParameter(VK_ACCESS_TOKEN_URL, VK_API_TOKEN)
                 .build();
 
-        URL url = null;
-        try {
-            url = new URL(builtUri.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        URL url = new URL(builtUri.toString());
         return url;
     }
 }
